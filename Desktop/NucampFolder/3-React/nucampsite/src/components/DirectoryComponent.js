1f257e900957bbb4f06777d57cfc599ede7df0c3
scostaus <35430283+scostaus@users.noreply.github.com>
2021-05-19 16:04:58 -0500

    Presentational and Container Components

--- Desktop/NucampFolder/3-React/nucampsite/src/components/DirectoryComponent.js
+++ Desktop/NucampFolder/3-React/nucampsite/src/components/DirectoryComponent.js
@@ -1,19 +1,8 @@
 
-
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
 class Directory extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedCampsite: null
-        };
-    }
-
-    onCampsiteSelect(campsite) {
-        this.setState({selectedCampsite: campsite});
-    }
 
     renderSelectedCampsite(campsite) {
         if (campsite) {
@@ -29,12 +18,11 @@ class Directory extends Component {
         }
         return <div />;
     }
-
     render() {
         const directory = this.props.campsites.map(campsite => {
             return (
                 <div key={campsite.id} className="col-md-5 m-1">
-                    <Card onClick={() => this.onCampsiteSelect(campsite)}>
+                    <Card onClick={() => this.props.onClick(campsite.id)}>
                         <CardImg width="100%" src={campsite.image} alt={campsite.name} />
                         <CardImgOverlay>
                             <CardTitle>{campsite.name}</CardTitle>
@@ -43,21 +31,13 @@ class Directory extends Component {
                 </div>
             );
         });
-
         return (
             <div className="container">
                 <div className="row">
                     {directory}
                 </div>
-                <div className="row">
-                    <div className="col-md-5 m-1">
-                        {this.renderSelectedCampsite(this.state.selectedCampsite)}
-                    </div>
-                </div>
             </div>
         );
     }
 }
-
 export default Directory;
-
--- Desktop/NucampFolder/3-React/nucampsite/src/shared/campsites.js
+++ Desktop/NucampFolder/3-React/nucampsite/src/shared/campsites.js
@@ -1,3 +1,5 @@
+
+
 export const CAMPSITES = [
     {
         id: 0,
